83dc56e60f89405783c2adc8a0086879c97920e9
Iman Radjavi <radjavi@hotmail.com>
2021-02-15 09:13:02 +0100

    package models + travis

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import Models.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Path path = new Path(2);
+        System.out.println(path.a);
     }
 }
--- src/Models/Path.java
+++ src/Models/Path.java
@@ -1,5 +1,9 @@
 package Models;
 
 public class Path {
-    
+  public int a;
+
+  public Path(int a) {
+    this.a = a;
+  }
 }
