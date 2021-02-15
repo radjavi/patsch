108957e2833187018ddcfe321d24cb1141bb2901
Iman Radjavi <radjavi@localhost.localdomain>
2021-02-15 19:32:31 +0100

    working gradle configuration

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,10 +1,11 @@
+package app;
+
 import models.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] times = {1,2,3,4};
+        int[] times = { 1, 2, 3, 4 };
         PositionGraph graph = new PositionGraph(times);
         System.out.println(graph);
-        
     }
 }
