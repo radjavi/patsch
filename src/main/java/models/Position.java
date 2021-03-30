8dc1b5328989a969d94a15d37579d5eeeb6d276a
radjavi <radjavi@hotmail.com>
2021-03-30 12:56:33 +0200

    position cleanup

--- src/main/java/models/Position.java
+++ src/main/java/models/Position.java
@@ -1,6 +1,6 @@
 package models;
 
-public class Position implements Comparable<Position> {
+public class Position {
   private final int x;
   private final int y;
 
@@ -26,32 +26,8 @@ public class Position implements Comparable<Position> {
     return this.y;
   }
 
-  public int maxDeltaXY(Position p) {
-    int x1 = p.getX();
-    int y1 = p.getY();
-    int deltaX = Math.abs(this.getX() - x1);
-    int deltaY = Math.abs(this.getY() - y1);
-    return Math.max(deltaX, deltaY);
-  }
-
   public double euclideanDistance(Position p) {
     return Math.sqrt(Math.pow(p.getX() - this.getX(), 2) + Math.pow(p.getY() - this.getY(), 2));
-  }
-
-  @Override
-  public int compareTo(Position pos) {
-    if (this.getX() == pos.getX() && this.getY() == pos.getY())
-      return 0;
-    if (this.getX() > pos.getX())
-      return 1;
-    if (this.getX() == pos.getX()) {
-      if (this.getY() > pos.getY())
-        return 1;
-      else
-        return -1;
-    }
-
-    return -1;
   }
 
   @Override
